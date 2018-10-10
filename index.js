018275740cc34ef3f1e8bf676c9a8088be9a7c3e
Sophie Bannwarth <Sophie@Sophies-MacBook-Air.local>
2018-10-10 14:30:31 -0400

    change index

--- index.js
+++ index.js
@@ -25,5 +25,4 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  server.close()
+  .close()
