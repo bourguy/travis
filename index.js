75c4eddb4d2108824d92b4296383b9d245597966
Sophie Bannwarth <Sophie@Sophies-MacBook-Air.local>
2018-10-10 14:15:12 -0400

    all changes in

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 
 const app = express();
 
@@ -11,4 +11,17 @@ const server = app.listen(3000, () => {
 });
 
 module.exports = server;
-server.close();
+*/
+
+
+// a integrer
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- test/index-spec.js
+++ test/index-spec.js
@@ -4,5 +4,6 @@ const server = require('../index');
 describe('test', () => {
   it('should return a string', () => {
     expect('ci with travis').to.equal('ci with travis');
+    expect('champion').to.equal('champion');
   });
 });
